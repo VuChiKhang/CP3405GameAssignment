3b70610c3d406f1eca73fda6a649b47859a557ba
VuChiKhang <chiklhang.vu@my.jcu.edu.au>
2019-09-25 09:19:43 +0800

    Initial commit

---
+++ Assets/Scripts/HalfPointTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfPointTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+
+    void OnTriggerEnter()
+    {
+        LapCompleteTrig.SetActive(true);
+        HalfLapTrig.SetActive(false);
+    }
+}
